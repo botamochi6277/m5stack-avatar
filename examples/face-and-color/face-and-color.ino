640e0d2f56bb4a048727b0ece7363a92901d43f8
botamochi6277 <botamochi6277+git@gmail.com>
2025-01-19 15:19:28 +0900

    Fix classname

--- examples/face-and-color/face-and-color.ino
+++ examples/face-and-color/face-and-color.ino
@@ -27,7 +27,7 @@ bool isShowingQR = false;
 class MyCustomFace : public Face {
  public:
   MyCustomFace()
-      : Face(new UShapeMouth(44, 44, 0, 16), new BoundingRect(222, 160),
+      : Face(new ToonMouth1(44, 44, 0, 16), new BoundingRect(222, 160),
              // right eye, second eye arg is center position of eye
              new EllipseEye(32, 32, false), new BoundingRect(163, 64),
              //  left eye
